c88255dc667b6e0a089630668b19dd2a1fccb7c2
DIRECTORY\geer <pieter.vangeer@deltares.nl>
2023-05-25 11:25:33 +0200

    #41 Fix presentation of properties

---
+++ src/StakeholderAnalysis.Visualization/DocumentViewTemplateSelector.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using StakeholderAnalysis.Visualization.ViewModels.DocumentViews.OnionDiagramView;
+using StakeholderAnalysis.Visualization.ViewModels.DocumentViews.StakeholderTableView;
+using StakeholderAnalysis.Visualization.ViewModels.DocumentViews.TwoAxisDiagrams;
+using StakeholderAnalysis.Visualization.ViewModels.Properties.OnionDiagramProperties;
+using StakeholderAnalysis.Visualization.ViewModels.Properties.TwoAxisDiagramProperties;
+
+namespace StakeholderAnalysis.Visualization
+{
+    public class DocumentViewTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultDataTemplate { get; set; }
+
+        public DataTemplate OnionDiagramViewTemplate { get; set; }
+
+        public DataTemplate StakeholderTableViewTemplate { get; set; }
+
+        public DataTemplate StakeholderForcesDiagramTemplate { get; set; }
+
+        public DataTemplate AttitudeImpactDiagramTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            switch (item)
+            {
+                case OnionDiagramViewModel _:
+                    return OnionDiagramViewTemplate;
+                case StakeholderTableViewModel _:
+                    return StakeholderTableViewTemplate;
+                case ForceFieldDiagramViewModel _:
+                    return StakeholderForcesDiagramTemplate;
+                case AttitudeImpactDiagramViewModel _:
+                    return AttitudeImpactDiagramTemplate;
+                default:
+                    return DefaultDataTemplate;
+            }
+        }
+    }
+}
---
+++ src/StakeholderAnalysis.Visualization/PropertiesViewTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using StakeholderAnalysis.Visualization.ViewModels.Properties.OnionDiagramProperties;
+using StakeholderAnalysis.Visualization.ViewModels.Properties.TwoAxisDiagramProperties;
+
+namespace StakeholderAnalysis.Visualization
+{
+    public class PropertiesViewTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DefaultDataTemplate { get; set; }
+
+        public DataTemplate OnionDiagramPropertiesTemplate { get; set; }
+
+        public DataTemplate TwoAxisDiagramPropertiesTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is OnionDiagramPropertiesViewModel) return OnionDiagramPropertiesTemplate;
+            if (item is TwoAxisDiagramPropertiesViewModel) return TwoAxisDiagramPropertiesTemplate;
+
+            return DefaultDataTemplate;
+        }
+    }
+}
