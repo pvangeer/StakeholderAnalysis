0acaadeb2e670d4e5b0b1280e86029aca1870a50
DIRECTORY\geer <pieter.vangeer@deltares.nl>
2023-06-08 10:46:03 +0200

    #44 Cleanup AnalysisServices

--- src/StakeholderAnalysis.Data/AnalysisServices.cs
+++ src/StakeholderAnalysis.Data/AnalysisServices.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Linq;
-using StakeholderAnalysis.Data.AttitudeImpactDiagrams;
-using StakeholderAnalysis.Data.ForceFieldDiagrams;
-using StakeholderAnalysis.Data.OnionDiagrams;
 
 namespace StakeholderAnalysis.Data
 {
@@ -39,27 +36,6 @@ namespace StakeholderAnalysis.Data
 
         public static void AddStakeholderToDiagram(IStakeholderDiagram diagram, Stakeholder stakeholder)
         {
-            switch (diagram)
-            {
-                case OnionDiagram onionDiagram:
-                    AddStakeholderToDiagram(onionDiagram, stakeholder);
-                    break;
-                case ForceFieldDiagram forceFieldDiagram:
-                    AddStakeholderToDiagram(forceFieldDiagram, stakeholder);
-                    break;
-                case AttitudeImpactDiagram attitudeImpactDiagram:
-                    AddStakeholderToDiagram(attitudeImpactDiagram, stakeholder);
-                    break;
-                default:
-                    throw new NotImplementedException();
-            }
-        }
-
-        #region AddStakeholdersToDiagrams
-        // TODO: Introduce interface to avoid multiple implementations of the same thing.
-
-        private static void AddStakeholderToDiagram(OnionDiagram diagram, Stakeholder stakeholder)
-        {
                 if (diagram.Stakeholders.All(s => s.Stakeholder != stakeholder))
                 {
                     var onionDiagramStakeholder = new PositionedStakeholder(stakeholder, 0.5, 0.5) { Rank = diagram.Stakeholders.Count };
@@ -69,7 +45,7 @@ namespace StakeholderAnalysis.Data
                 }
         }
 
-        private static void FindPositionForNewOnionDiagramStakeholder(OnionDiagram diagram, PositionedStakeholder stakeholder, int count = 1)
+        private static void FindPositionForNewOnionDiagramStakeholder(IStakeholderDiagram diagram, PositionedStakeholder stakeholder, int count = 1)
         {
             if (count > 200)
             {
@@ -83,74 +59,12 @@ namespace StakeholderAnalysis.Data
             {
                 var newLeft = stakeholder.Left + 0.01;
                 stakeholder.Left = newLeft > 1.0 ? newLeft - 1.0 : newLeft;
+
                 var newTop = stakeholder.Top + 0.02;
                 stakeholder.Top = newTop > 1.0 ? newTop - 1.0 : newTop;
+
                 FindPositionForNewOnionDiagramStakeholder(diagram, stakeholder, count);
             }
         }
-
-        private static void AddStakeholderToDiagram(ForceFieldDiagram diagram, Stakeholder stakeholder)
-        {
-            if (diagram.Stakeholders.All(s => s.Stakeholder != stakeholder))
-            {
-                var diagramStakeholder = new PositionedStakeholder(stakeholder, 0.5, 0.5) { Rank = diagram.Stakeholders.Count };
-                FindPositionForNewStakeholder(diagram, diagramStakeholder);
-                diagram.Stakeholders.Add(diagramStakeholder);
-            }
-        }
-
-        private static void FindPositionForNewStakeholder(ForceFieldDiagram diagram, PositionedStakeholder stakeholder, int count = 1)
-        {
-            if (count > 200)
-            {
-                return;
-            }
-
-            count += 1;
-
-            if (diagram.Stakeholders.Any(s =>
-                    Math.Abs(s.Top - stakeholder.Top) < 1E-3 & Math.Abs(s.Left - stakeholder.Left) < 1E-3))
-            {
-                var newTop = stakeholder.Top + 0.02;
-                stakeholder.Top = newTop > 1.0 ? newTop - 1.0 : newTop;
-                
-                var newLeft = stakeholder.Left + 0.01;
-                stakeholder.Left = newLeft > 1.0 ? newLeft - 1.0 : newLeft;
-
-                FindPositionForNewStakeholder(diagram, stakeholder, count);
-            }
-        }
-
-        private static void AddStakeholderToDiagram(AttitudeImpactDiagram diagram, Stakeholder stakeholder)
-        {
-            if (diagram.Stakeholders.All(s => s.Stakeholder != stakeholder))
-            {
-                var attitudeImpactDiagramStakeholder = new PositionedStakeholder(stakeholder, 0.5, 0.5) { Rank = diagram.Stakeholders.Count };
-                FindPositionForNewStakeholder(diagram, attitudeImpactDiagramStakeholder);
-                diagram.Stakeholders.Add(attitudeImpactDiagramStakeholder);
-            }
-        }
-
-        private static void FindPositionForNewStakeholder(AttitudeImpactDiagram diagram, PositionedStakeholder stakeholder, int count = 1)
-        {
-            if (count > 200)
-            {
-                return;
-            }
-
-            count += 1;
-
-            if (diagram.Stakeholders.Any(s =>
-                    Math.Abs(s.Top - stakeholder.Top) < 1E-3 & Math.Abs(s.Left - stakeholder.Left) < 1E-3))
-            {
-                var newTop = stakeholder.Top - 0.02;
-                stakeholder.Top = newTop < 0.0 ? newTop + 1.0 : newTop;
-                var newLeft = stakeholder.Left + 0.01;
-                stakeholder.Left = newLeft > 1.0 ? newLeft - 1.0 : newLeft;
-                FindPositionForNewStakeholder(diagram, stakeholder, count);
-            }
-        }
-
-        #endregion
     }
 }
